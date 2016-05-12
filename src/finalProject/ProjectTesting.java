85d22297355a990008b6fee6c71471b560bf71c1
unknown <hgoldstein169@FHS-LAB127-LE27.fhs.win.fuhsd.org>
2016-05-12 09:03:25 -0700

    make test file with sine wave

--- src/finalProject/ProjectTesting.java
+++ src/finalProject/ProjectTesting.java
@@ -2,57 +2,38 @@ package finalProject;
 
 import processing.core.PApplet;
 
-public class ProjectTesting extends PApplet{
-	
-	
-	
-	public void setup(){
+public class ProjectTesting extends PApplet {
+
+	int middle;
+
+	public void setup() {
 		size(600, 600);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-	
-	public void draw(){
-		
-		
-		drawLine(10, 100, 20);
-		
-		
-		
-		
-		
-		
-		
 
+		middle = 300;
 
-		
 	}
-	
-	public void drawLine( int x, int shift, int nextAmount){
-		if(x == 600 - nextAmount){
+
+	public void draw() {
+
+		drawLine(10, 100, 5, 30);
+
+	}
+
+	public void drawLine(int x, int shift, int nextAmount, double width) {
+		if (x == 600 - nextAmount) {
 			return;
 		}
-		
+
 		int x1 = x;
 		int x2 = x + nextAmount;
-		
-		int y1 = (int) (Math.sin(x1) * shift) + 300;
-		int y2 = (int) (Math.sin(x2) * shift) + 300;
-		
+
+		int y1 = (int) (Math.sin(x1 / width) * shift) + 300;
+		int y2 = (int) (Math.sin(x2 / width) * shift) + 300;
+
 		line(x1, y1, x2, y2);
-		
-		drawLine( x2, shift, nextAmount);
-		
+
+		drawLine(x2, shift, nextAmount, width);
+
 	}
-	
-	
 
 }
